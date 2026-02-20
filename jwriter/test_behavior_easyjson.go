9715e5410011775ee68f0133ab49d4a8b9014acc
Matthew Keeler <mkeeler@launchdarkly.com>
2026-02-20 16:38:12 -0500

    bump golangci-lint

--- jwriter/test_behavior_easyjson.go
+++ jwriter/test_behavior_easyjson.go
@@ -5,6 +5,6 @@ package jwriter
 
 // This function tells the writer tests that we should expect to see hex escape sequences in the output
 // for certain characters, because that's the behavior of easyjson.
-func tokenWriterWillEncodeAsHex(ch rune) bool { //nolint:deadcode,unused // linter is confused
+func tokenWriterWillEncodeAsHex(ch rune) bool { //nolint:unused // linter is confused
 	return ch != '\t' && ch != '\n' && ch != '\r'
 }
